107e3039fae58e688660a7a5c455c099407e921f
Apzhyn <apzhyn@noreply.codeberg.org>
2026-05-16 21:31:17 +0200

    Add libs/zlib/build.sh

---
+++ libs/zlib/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+export PATH="$HOME/staticlinux/toolchain/bin:$PATH"
+VERSION="1.3.1"
+wget https://zlib.net/zlib-$VERSION.tar.gz
+tar xf zlib-$VERSION.tar.gz
+cd zlib-$VERSION
+CC=x86_64-linux-musl-gcc ./configure --static --prefix=/usr
+make -j$(nproc)
