1b14fa783e47e3c74ebe26eb404075db86d78502
Apzhyn <apzhyn@noreply.codeberg.org>
2026-05-16 16:45:39 +0200

    Update shells/dash/build.sh

--- shells/dash/build.sh
+++ shells/dash/build.sh
@@ -8,5 +8,5 @@ VERSION="0.5.12"
 wget http://gondor.apana.org.au/~herbert/dash/files/dash-$VERSION.tar.gz
 tar xf dash-$VERSION.tar.gz
 cd dash-$VERSION
-CC=x86_64-linux-musl-gcc ./configure --host=x86_64-linux-musl --enable-static
+CC=x86_64-linux-musl-clang ./configure --host=x86_64-linux-musl --enable-static
 make -j$(nproc)
