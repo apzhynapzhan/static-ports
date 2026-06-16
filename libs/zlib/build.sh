96e52431074dcb8b750b61d1689a38f747fcf513
situation <git@apzhyn.xyz>
2026-06-16 19:25:58 +0200

    cleanup

--- libs/zlib/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-export PATH="$HOME/staticlinux/toolchain/bin:$PATH"
-VERSION="1.3.2"
-wget https://zlib.net/zlib-$VERSION.tar.gz
-tar xf zlib-$VERSION.tar.gz
-cd zlib-$VERSION
-CC=x86_64-linux-musl-gcc ./configure --static --prefix=/usr
-make -j$(nproc)
